5edd259f7df33a6fc4bdde1133291cc143d49de6
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-04-29 14:55:20 +0300

    Add root route to webapp

--- rustbridge/src/main.rs
+++ rustbridge/src/main.rs
@@ -5,7 +5,13 @@ use pencil::{Request, PencilResult, Response};
 use pencil::method::Get;
 
 
+fn hello(_: &mut Request) -> PencilResult {
+    Ok(Response::from("Hello RustBridge!"))
+}
+
+
 fn main() {
     let mut app = Pencil::new("/web/hello");
+    app.route("/", &[Get], "hello", hello);
     app.run("0.0.0.0:5000");
 }
