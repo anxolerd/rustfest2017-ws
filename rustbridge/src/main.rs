faeabe4036318e84b74a0c4459b75c2ba8f67b94
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-04-29 15:24:41 +0300

    Display random motivation on main page

--- rustbridge/src/main.rs
+++ rustbridge/src/main.rs
@@ -1,24 +1,33 @@
 extern crate pencil;
+extern crate pick_one;
+extern crate motivations;
+
 
 use pencil::Pencil;
 use pencil::{Request, PencilResult, Response};
 use pencil::method::Get;
 
 
-fn hello(_: &mut Request) -> PencilResult {
-    Ok(Response::from("Hello RustBridge!"))
+fn get_motivation() -> &'static str {
+    return pick_one::pick_one_str(&motivations::MOTIVATIONS);
+}
+
+
+fn motivation(_: &mut Request) -> PencilResult {
+    let motivation = get_motivation();
+    return Ok(Response::from(motivation));
 }
 
 
 fn hello_name(r: &mut Request) -> PencilResult {
     let name = r.view_args.get("name").unwrap();
-    Ok(format!("Hello, {}!", name).into())
+    return Ok(format!("Hello, {}!", name).into())
 }
 
 
 fn main() {
     let mut app = Pencil::new("/web/hello");
-    app.route("/", &[Get], "hello", hello);
+    app.route("/", &[Get], "motivation", motivation);
     app.route("/hello/<name:string>", &[Get], "hello_name", hello_name);
     app.run("0.0.0.0:5000");
 }
