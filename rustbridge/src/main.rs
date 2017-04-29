dca7c11b1a9a8ff672bd68a7c04ef216884595aa
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-04-29 15:08:39 +0300

    Add route with param

--- rustbridge/src/main.rs
+++ rustbridge/src/main.rs
@@ -10,8 +10,15 @@ fn hello(_: &mut Request) -> PencilResult {
 }
 
 
+fn hello_name(r: &mut Request) -> PencilResult {
+    let name = r.view_args.get("name").unwrap();
+    Ok(format!("Hello, {}!", name).into())
+}
+
+
 fn main() {
     let mut app = Pencil::new("/web/hello");
     app.route("/", &[Get], "hello", hello);
+    app.route("/hello/<name:string>", &[Get], "hello_name", hello_name);
     app.run("0.0.0.0:5000");
 }
